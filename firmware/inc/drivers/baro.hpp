ec0d54f142dc6ad89dea34ed2fd600ca341c10b0
Alex <84359295+salt4king@users.noreply.github.com>
2026-09-01 11:01:29 -0700

    Update baro.hpp

--- firmware/inc/drivers/baro.hpp
+++ firmware/inc/drivers/baro.hpp
@@ -3,12 +3,13 @@
 
 Class MS5611 {
 public:
+  
 
 private:
   int spiChannel = 0; // change once we assign pin
   int csPin = 0; // Change once we assign a pin
 
-  enum class OSR : uint8_t { // found from command data sheet OSR (8 bits per) pg 10
+  enum class OSR: uint8_t { // found from command data sheet OSR (8 bits per) pg 10
         OSR_256  = 0x40,
         OSR_512  = 0x42,
         OSR_1024 = 0x44,
@@ -31,5 +32,9 @@ private:
 
   float pressureDiff = 0.0f;               // dT
   float actualTemp = 0.0f;                 // Temp
+
+  int64_t tempOffSet();                    // OFF
+  int64_t tempSense();                     // SENS
+  int32_t tempCompPressure();              // P
   
 };
